bf3992023e3e5fad65df7283ddfe15dae7a26fdc
Kavi-Dew-23 <dewapuraka@gmail.com>
2024-07-28 18:32:29 +0530

    Updates Products page

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\bootstrap5\ActiveForm;
+use mihaildev\ckeditor\CKEditor;
 
 /** @var yii\web\View $this */
 /** @var common\models\Product $model */
@@ -14,11 +15,19 @@ use yii\bootstrap5\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'description')->widget(CKEditor::class,[
+    'editorOptions' => [
+        'preset' => 'basic', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
+        'inline' => false, //по умолчанию false
+    ],
+    ]); 
+    ?>
 
-    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'image')->fileInput() ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?= $form->field($model, 'price')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'status')->checkbox() ?>
 
     
 
